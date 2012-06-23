2d3b147f080de3713877ceeddcccc4803d8bb93e
Amos Ng <amos@piazza.com>
2012-06-22 20:43:43 -0700

    Changed general layout template and added requirements to events.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ActiveRecord::Base
   attr_accessible :content, :date, :title
+  
+  validates :title,  :presence => true
+  validates :date,   :presence => true
+  validates :content,  :presence => true,
+                     :length => { :minimum => 5 }
 end
