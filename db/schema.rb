b25923328bd08c9772a2c33d306541a117e81507
Amos Ng <amos@piazza.com>
2012-06-23 16:10:31 -0700

    Added jQuery datepicker to the Event form.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120623155444) do
+ActiveRecord::Schema.define(:version => 20120623221649) do
 
   create_table "events", :force => true do |t|
     t.string   "title"
-    t.string   "date"
+    t.date     "date"
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
