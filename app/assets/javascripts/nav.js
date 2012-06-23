d311ad318cd38665be7472e2e9f76b6c152e5952
Amos Ng <amos@piazza.com>
2012-06-23 12:28:34 -0700

    Made website "softer" with rounded corners.
    "Events" tab looks weird if "Officers" tab is selected and "Events" is hovered over, thus prompting me to extend the length of hovered-over tabs. That, however, results in tabs overlapping with the content div.

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
   // I have a feeling this is a very stupid way to do things
   var path = window.location.pathname;
-  if (path.indexOf("officers") >= 0) $("#officers_nav").attr("id","selected");
-  if (path.indexOf("events") >= 0) $("#events_nav").attr("id","selected");
+  officers = path.indexOf("officers") >= 0;
+  events = path.indexOf("events") >= 0;
+  if (officers) $("#officers_nav").attr("id","selected");
+  if (events) {
+  	$("#events_nav").attr("id","selected");
+  	$("#content").css("border-top-left-radius","0 0")
+  }
 })
