f75db14a10aa46d42e4e841c7e1b87a092d31670
Amos Ng <amos@piazza.com>
2012-06-24 11:24:35 -0700

    Added a time field in the Event form.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
-  attr_accessible :content, :time, :title, :date
+  attr_accessible :content, :time, :title, :date, :day_time
 
-  validates :title, :date, :content, :presence => true
+  validates :title, :date, :day_time, :content, :presence => true
   validates :content, :length => {:minimum => 5}
 
   def date
@@ -20,6 +20,25 @@ class Event < ActiveRecord::Base
     d = Date.strptime(day, '%m/%d/%Y')
     self.time = DateTime.new(d.year, d.month, d.day, current_time.hour,
                              current_time.min, current_time.sec,
+                             current_time.zone).to_time.to_i
+  end
+
+  def day_time
+    if time.nil?
+      return Time.now.beginning_of_hour.strftime('%I:%M %p')
+    else
+      return Time.at(time).strftime('%I:%M %p')
+    end
+  end
+
+  def day_time=(dt)
+    current_time = Time.at(0).to_datetime
+    unless time.nil?
+      current_time = Time.at(time).to_datetime
+    end
+    t = DateTime.strptime(current_time.strftime('%Y %m %d ') + dt, '%Y %m %d %I:%M %p')
+    self.time = DateTime.new(current_time.year, current_time.month,
+                             current_time.day, t.hour, t.min, 0,
                              current_time.zone).to_time.to_i
   end
 
