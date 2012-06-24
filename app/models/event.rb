2f50aa8a74b9e3b31b1b4c92c18792cc8dc98476
Amos Ng <amos@piazza.com>
2012-06-24 10:04:13 -0700

    Got the Date field to successfully edit the Time field.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,27 @@
 class Event < ActiveRecord::Base
-  attr_accessible :content, :time, :title
+  attr_accessible :content, :time, :title, :date
 
-  validates :title, :time, :content, :presence => true
+  validates :title, :date, :content, :presence => true
   validates :content, :length => {:minimum => 5}
+
+  def date
+    if time.nil?
+      return Time.now.strftime('%m/%d/%Y')
+    else
+      Time.at(time).strftime('%m/%d/%Y')
+    end
+  end
+
+  def date=(day)
+    current_time = Time.at(0).to_datetime
+    unless time.nil?
+      current_time = Time.at(time).to_datetime
+    end
+    d = Date.strptime(day, '%m/%d/%Y')
+    self.time = DateTime.new(d.year, d.month, d.day, current_time.hour,
+                             current_time.min, current_time.sec,
+                             current_time.zone).to_time.to_i
+  end
 
   def summary
     max_length = 300
@@ -14,6 +33,6 @@ class Event < ActiveRecord::Base
   end
 
   def human_time
-    return Time.at(time).strftime('%A, %B %e at %l:%M %p')
+    Time.at(time).strftime('%A, %B %e at %l:%M %p')
   end
 end
