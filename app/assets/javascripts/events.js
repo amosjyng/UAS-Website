55e4e7acc09f2a739855ac518f189773aa122800
Amos Ng <amos@piazza.com>
2012-06-23 17:17:19 -0700

    Cleaned up code and fixed a bug where datepicker was still visible before date field is first clicked.

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,3 +1,4 @@
 $(document).ready(function () {
   $("#event_date").datepicker({minDate: new Date()});
+  $("#ui-datepicker-div").hide();
 });
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ActiveRecord::Base
   attr_accessible :content, :date, :title
+
+  validates :title, :date, :content, :presence => true
+  validates :content, :length => {:minimum => 5}
 end
