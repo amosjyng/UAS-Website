3f239a2d86f7e2f7a07441ca071c98855984b246
Amos Ng <amos@piazza.com>
2012-06-23 17:37:51 -0700

    Modified the Events overview to list all Events as posts.

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,12 @@ class Event < ActiveRecord::Base
 
   validates :title, :date, :content, :presence => true
   validates :content, :length => {:minimum => 5}
+
+  def summary
+    if content.length > 100
+      return content[0..300] + '...'
+    else
+      return content
+    end
+  end
 end
