4b763ff3dfa779f34a78977d54499a18cfeb7e0d
Amos Ng <amos@piazza.com>
2012-06-23 19:22:45 -0700

    Turns out the piece of Javascript wasn't working correctly in the first place (missing #), and it was unneeded because the CSS was doing a fine job patching up the upper-left hand corner.

--- app/assets/javascripts/nav.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  if ($("selected").children().html() == 'Events') {
-  	// we don't want the "Events" tab to look like it's not connected
-  	// to the body
-  	$("#content").css("border-top-left-radius","0 0")
-  }
-})
