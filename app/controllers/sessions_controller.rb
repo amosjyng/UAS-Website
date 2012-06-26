f9b8ec30c7077d78b2108e46a3fca956470c9924
Amos Ng <amos@piazza.com>
2012-06-25 22:24:11 -0700

    Added errors for the sign-in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,10 +9,14 @@ class SessionsController < ApplicationController
   def create
     @officer = Officer.where("email = ?", params[:session][:email]).first
     respond_to do |format|
-      if !@officer.nil? && @officer.authenticate(params[:session][:password])
-          format.html { redirect_to @officer, :notice => 'You\'ve signed in!' }
-      else
+      if @officer.nil?
+        flash[:error] = "You're using the wrong e-mail to log in!"
         format.html { render :action => 'new' }
+      elsif !@officer.authenticate params[:session][:password] 
+        flash[:error] = "You've entered the wrong password!"
+        format.html { render :action => 'new' }
+      else
+        format.html { redirect_to @officer, :notice => "You've signed in!" }
       end
     end
   end
