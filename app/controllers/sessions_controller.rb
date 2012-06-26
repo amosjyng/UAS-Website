95531fb8a6d05f84f7d07eefef4a7ea6bf7ef942
Amos Ng <amos@piazza.com>
2012-06-25 22:26:20 -0700

    Fixed flash bug, where flash would stay for one extra refresh.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,10 +10,10 @@ class SessionsController < ApplicationController
     @officer = Officer.where("email = ?", params[:session][:email]).first
     respond_to do |format|
       if @officer.nil?
-        flash[:error] = "You're using the wrong e-mail to log in!"
+        flash.now[:error] = "You're using the wrong e-mail to log in!"
         format.html { render :action => 'new' }
       elsif !@officer.authenticate params[:session][:password] 
-        flash[:error] = "You've entered the wrong password!"
+        flash.now[:error] = "You've entered the wrong password!"
         format.html { render :action => 'new' }
       else
         format.html { redirect_to @officer, :notice => "You've signed in!" }
