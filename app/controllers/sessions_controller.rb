26e9aca24ab86a396eb0c9927c7ae44ac55630d5
Amos Ng <amos@piazza.com>
2012-06-26 20:46:02 -0700

    Added Officer sign-in link to officers index page.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,4 @@
 class SessionsController < ApplicationController
-  def new
-    respond_to do |format|
-      format.html # new.html.erb
-      # what to do with json? I'm not really sure
-    end
-  end
 
   def create
     @officer = Officer.where("email = ?", params[:session][:email]).first
